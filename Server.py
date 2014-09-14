dab61aae8a6d02e42181d47ad85ee35b2d4df849
Restioson <cael.marquard@gmail.com>
2014-09-14 08:56:47 +0200

    Documented Server.py
    Phew... lots of code!

--- Server.py
+++ Server.py
@@ -1,27 +1,40 @@
+#Server for Airchat
+
+#Imports
 from socket import *
 import _thread as thread
 import time
 
+#Set the buffersize, hostname and port
 BUFF = 1024
 HOST = '0.0.0.0'
 PORT = 65000
+
+#Create a username list and socket list
 list=[]
 ul = []
 
+#Handler for sockets
 def handler(clientsock,addr):
 	joinnew = True
+	#Add them to the list of sockets
 	list.append(clientsock)
+	#Set their default username (hostname)
 	USERNAME = "<"+gethostbyaddr(addr[0])[0]+">"
 	while 1:
+		#If they disconnect, remove thier username from the list and also thier socket
 		try:
 			data = str(clientsock.recv(BUFF))
 		except:
 			list.pop(list.index(clientsock))
 			ul.pop(ul.index(USERNAME))
 			break
+		
+		#Send the other clients the current socket's messages
 		for i in list:
+			#If the client is new:
 			if joinnew == True:
-				i.send((USERNAME+" joined the server.").encode('utf-8'))
+				i.send((USERNAME+" joined the server.").encode('utf-8')) #Say they just joined
 				joinnew = False
 			repr(list)
 			print('data:' + repr(data))
@@ -30,6 +43,7 @@ def handler(clientsock,addr):
 				break
 			if "~" not in data:
 				i.send((USERNAME+":"+data).encode('utf-8'))	
+			#Change thier name if there is a '~' in front
 			if "~" in data:
 				try:
 					USERNAME = ("<"+data.replace("b'",'',1).replace("'",'').replace('~','',1)+">".replace(' ','',1))
@@ -42,6 +56,7 @@ def handler(clientsock,addr):
 				except:
 					clientsock.send("<Alert> Error.".encode("utf-8"))
 
+#Run server
 
 if __name__=='__main__':
 	ADDR = (HOST, PORT)
@@ -50,6 +65,7 @@ if __name__=='__main__':
 	serversock.bind(ADDR)
 	serversock.listen(5)
 	while 1:
+		#Mainloop
 		print('waiting for connection...')
 		clientsock, addr = serversock.accept()
 		print ('...connected from:', addr)
